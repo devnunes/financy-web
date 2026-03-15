3e58ffafd5dcd3fdb3a6615030d84ce69467dc61
Matheus Nunes <matheusnunesinfo@gmail.com>
2026-03-15 00:13:11 -0300

    feat: update Apollo Client configuration to use dynamic GraphQL URI and add server proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3333',
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
